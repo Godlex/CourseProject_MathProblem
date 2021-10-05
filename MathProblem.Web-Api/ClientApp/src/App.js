b182a1792365fc4244dc6a81a42bcd89961755c4
Alex <alexei.truhan@gmail.com>
2021-10-05 10:18:37 +0300

    add darck and light theme

--- MathProblem.Web-Api/ClientApp/src/App.js
+++ MathProblem.Web-Api/ClientApp/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Route} from 'react-router';
 import {Layout} from './components/Layout';
 import {Home} from './components/Home';
@@ -9,15 +9,30 @@ import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizat
 import {ApplicationPaths} from './components/api-authorization/ApiAuthorizationConstants';
 
 import './custom.css'
+import { useDarkMode } from './useDarkMode';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import Toggle from './components/Toggle';
 
 export default function App() {
+
+    const [theme, toggleTheme] = useDarkMode();
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+    
     return (
-        <Layout>
-            <Route exact path='/' component={Home}/>
-            <Route path='/counter' component={Counter}/>
-            <AuthorizeRoute path='/fetch-data' component={FetchData}/>
-            <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes}/>
-        </Layout>
+        <ThemeProvider theme={themeMode}>
+                <Layout>
+                    <GlobalStyles />
+                    <Toggle theme={theme} toggleTheme={toggleTheme} />
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/counter' component={Counter}/>
+                    <AuthorizeRoute path='/fetch-data' component={FetchData}/>
+                    <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes}/>
+                </Layout>
+          
+        </ThemeProvider>
+        
     );
 }
     
