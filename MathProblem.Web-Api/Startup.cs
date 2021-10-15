be2013cf131bb614be3d4f5b5e1a96d0d2b7fe86
Alex <alexei.truhan@gmail.com>
2021-10-15 21:12:46 +0300

    add EnforceHttpsMiddleware.cs

--- MathProblem.Web-Api/Startup.cs
+++ MathProblem.Web-Api/Startup.cs
@@ -80,6 +80,7 @@ namespace MathProblem.Web_Api
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseReverseProxyHttpsEnforcer();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
