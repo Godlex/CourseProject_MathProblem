459db747a307a7930f342da6457e985369048ded
Alex <alexei.truhan@gmail.com>
2021-10-17 13:50:58 +0300

    add Pomelo.EntityFrameworkCore.MySql

--- MathProblem.Web-Api/Startup.cs
+++ MathProblem.Web-Api/Startup.cs
@@ -1,17 +1,17 @@
 namespace MathProblem.Web_Api
 {
+    using System;
     using Contracts.Queries;
     using DAL;
     using MediatR;
     using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
 
     public class Startup
     {
@@ -23,13 +23,13 @@ namespace MathProblem.Web_Api
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        
-        public void ConfigureServices(IServiceCollection services) 
+
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseMySQL(  
-                    Configuration.GetConnectionString("DefaultConnection")));
-            
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
+                    new MySqlServerVersion(new Version(5, 6, 0))));
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
@@ -45,19 +45,16 @@ namespace MathProblem.Web_Api
             services.AddRazorPages();
 
             services.AddMediatR(typeof(GetUserProfileByUserId).Assembly);
-            
+
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/build";
-            });
-            
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+
             services.AddAuthentication().AddFacebook(facebookOptions =>
             {
                 facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
                 facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
             });
-            
+
             services.AddAuthentication()
                 .AddGoogle(options =>
                 {
@@ -83,7 +80,7 @@ namespace MathProblem.Web_Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
+
             /*app.UseReverseProxyHttpsEnforcer();*/
 
             app.UseHttpsRedirection();
